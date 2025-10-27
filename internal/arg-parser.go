ebe95090e9dfd1e381586130f48755dc0e4541c5
Tim Wang <tianxun.wang1@nhs.net>
2025-10-27 10:16:57 +0000

    added version number to build

--- internal/arg-parser.go
+++ internal/arg-parser.go
@@ -1,88 +1,88 @@
 package internal
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"path"
-	"runtime"
-	"strings"
+    "flag"
+    "fmt"
+    "os"
+    "path"
+    "runtime"
+    "strings"
 
-	"gopkg.in/ini.v1"
+    "gopkg.in/ini.v1"
 )
 
 func Usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s <role> [<command> <args...>]\n", os.Args[0])
-	flag.PrintDefaults()
+    fmt.Fprintf(os.Stderr, "Usage: %s <role> [<command> <args...>]\n", os.Args[0])
+    flag.PrintDefaults()
 }
 
 func defaultFormat() string {
-	shell := os.Getenv("SHELL")
+    shell := os.Getenv("SHELL")
 
-	switch runtime.GOOS {
-	case "windows":
-		if shell == "" {
-			return "powershell"
-		}
-		fallthrough
-	default:
-		if strings.HasSuffix(shell, "fish") {
-			return "fish"
-		}
-		return "bash"
-	}
+    switch runtime.GOOS {
+    case "windows":
+        if shell == "" {
+            return "powershell"
+        }
+        fallthrough
+    default:
+        if strings.HasSuffix(shell, "fish") {
+            return "fish"
+        }
+        return "bash"
+    }
 }
 
 func ParseArgs(versionNumber string) (string, int, *ini.File, string) {
-	homeDir, _ := os.UserHomeDir()
-	defaultCredentialsFilepath := path.Join(homeDir, ".aws", "credentials")
-	defaultConfigFilepath := path.Join(homeDir, ".aws", "config")
+    homeDir, _ := os.UserHomeDir()
+    defaultCredentialsFilepath := path.Join(homeDir, ".aws", "credentials")
+    defaultConfigFilepath := path.Join(homeDir, ".aws", "config")
 
-	duration := flag.Int("duration", 3600, "The duration that the credentials will be valid for in seconds.")
-	credentialsFilepath := flag.String("credentials-path", defaultCredentialsFilepath, "The absolute filepath to your aws credentials file.")
-	configFilepath := flag.String("config-path", defaultConfigFilepath, "The absolute filepath to your aws config file.")
-	format := flag.String("format", defaultFormat(), "Format can be \"bash\", \"fish\" or \"powershell\".")
-	list := flag.Bool("list", false, "Show list of available roles.")
-	version := flag.Bool("version", false, "Show version.")
+    duration := flag.Int("duration", 3600, "The duration that the credentials will be valid for in seconds.")
+    credentialsFilepath := flag.String("credentials-path", defaultCredentialsFilepath, "The absolute filepath to your aws credentials file.")
+    configFilepath := flag.String("config-path", defaultConfigFilepath, "The absolute filepath to your aws config file.")
+    format := flag.String("format", defaultFormat(), "Format can be \"bash\", \"fish\" or \"powershell\".")
+    list := flag.Bool("list", false, "Show list of available roles.")
+    version := flag.Bool("version", false, "Show version.")
 
-	flag.Parse()
-	argv := flag.Args()
+    flag.Parse()
+    argv := flag.Args()
 
-	validateFormat(*format)
+    validateFormat(*format)
 
-	awsConfigFiles, err := ini.LooseLoad(*credentialsFilepath, *configFilepath)
-	CheckError(err, "Could not load aws credentials or config file")
+    awsConfigFiles, err := ini.LooseLoad(*credentialsFilepath, *configFilepath)
+    CheckError(err, "Could not load aws credentials or config file")
 
-	if *version {
-		fmt.Fprintf(os.Stderr, "%s\n", versionNumber)
-		os.Exit(0)
-	}
+    if *version {
+        fmt.Fprintf(os.Stderr, "%s\n", versionNumber)
+        os.Exit(0)
+    }
 
-	if *list {
-		fmt.Fprintf(os.Stderr, "Available AWS roles:\n")
-		for _, name := range awsConfigFiles.SectionStrings() {
-			fmt.Fprintf(os.Stderr, "%s\n", name)
-		}
-		os.Exit(0)
-	}
+    if *list {
+        fmt.Fprintf(os.Stderr, "Available AWS roles:\n")
+        for _, name := range awsConfigFiles.SectionStrings() {
+            fmt.Fprintf(os.Stderr, "%s\n", name)
+        }
+        os.Exit(0)
+    }
 
-	if len(argv) < 1 {
-		flag.Usage()
-		os.Exit(1)
-	}
+    if len(argv) < 1 {
+        flag.Usage()
+        os.Exit(1)
+    }
 
-	return argv[0], *duration, awsConfigFiles, *format
+    return argv[0], *duration, awsConfigFiles, *format
 }
 
 func validateFormat(format string) {
-	validFormats := []string{"bash", "fish", "powershell"}
+    validFormats := []string{"bash", "fish", "powershell"}
 
-	for _, validFormat := range validFormats {
-		if validFormat == format {
-			return
-		}
-	}
+    for _, validFormat := range validFormats {
+        if validFormat == format {
+            return
+        }
+    }
 
-	flag.Usage()
-	os.Exit(1)
+    flag.Usage()
+    os.Exit(1)
 }
